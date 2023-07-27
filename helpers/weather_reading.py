774b8fbca3aace3967a37e24d6113ae2820c48ad
Maryam Shahid <maryam.shahid@A006-01261.local>
2023-07-27 14:09:37 +0500

    separated code into files

---
+++ helpers/weather_reading.py
@@ -0,0 +1,11 @@
+class WeatherReading:
+
+    def __init__(self, day, max_temp, mean_temp,
+                 min_temp, mean_humidity):
+        self.day = day
+        self.max_temp = max_temp
+        self.mean_temp = mean_temp
+        self.min_temp = min_temp
+        self.mean_humidity = mean_humidity
+
+
